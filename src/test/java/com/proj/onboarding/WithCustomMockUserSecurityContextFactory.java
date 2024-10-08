892500a87decfd797bba07da2f83e8d29c85db65
Lys <redspinyant@gmail.com>
2024-10-08 18:10:24 +0900

    test : JWT, User 테스트 코드 작성

---
+++ src/test/java/com/proj/onboarding/WithCustomMockUserSecurityContextFactory.java
@@ -0,0 +1,29 @@
+package com.proj.onboarding;
+
+import java.util.List;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.test.context.support.WithSecurityContextFactory;
+
+import com.proj.onboarding.domain.auth.entity.UserRoleType;
+
+public class WithCustomMockUserSecurityContextFactory implements WithSecurityContextFactory<WithCustomMockUser> {
+
+	@Override
+	public SecurityContext createSecurityContext(WithCustomMockUser annotation) {
+
+		String username = annotation.username();
+
+		Authentication auth = new UsernamePasswordAuthenticationToken(username, "",
+			List.of(new SimpleGrantedAuthority(UserRoleType.USER.getRoleName())));
+
+		SecurityContext context = SecurityContextHolder.getContext();
+		context.setAuthentication(auth);
+
+		return context;
+	}
+}
