7ce368fd5ce8ebc01f214253834fd44fadd5bb9c
Lys <redspinyant@gmail.com>
2024-10-08 19:06:00 +0900

    feat : Swagger UI 설정
    접근 URL 권한 허용으로 변경,
    Config 추가,
    설명 추가

--- src/main/java/com/proj/onboarding/domain/auth/UserController.java
+++ src/main/java/com/proj/onboarding/domain/auth/UserController.java
@@ -11,22 +11,27 @@ import com.proj.onboarding.domain.auth.dto.SignResponseDto;
 import com.proj.onboarding.domain.auth.dto.SignupRequestDto;
 import com.proj.onboarding.domain.auth.dto.SignupResponseDto;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 
 @Controller
 @RequestMapping("/api")
 @RequiredArgsConstructor
+@Tag(name = "Users", description = "User API")
 public class UserController {
 
 	private final UserService userService;
 
 	@PostMapping("/signup")
+	@Operation(summary = "회원가입", description = "회원가입 할 때 사용하는 API")
 	public ResponseEntity<SignupResponseDto> signup(@RequestBody SignupRequestDto requestDto) {
 		SignupResponseDto response = userService.signup(requestDto);
 		return ResponseEntity.ok().body(response);
 	}
 
 	@PostMapping("/sign")
+	@Operation(summary = "로그인", description = "로그인에 사용하는 API")
 	public ResponseEntity<SignResponseDto> sign(@RequestBody SignRequestDto requestDto) {
 		SignResponseDto response = userService.sign(requestDto);
 		return ResponseEntity.ok().body(response);
